d5e70165e11d1fa4c74601a3ff521f0042b8d25e
soundreaper <soundreaper@gmail.com>
2020-04-13 01:46:23 -0700

    add two files for code syntax checking

---
+++ duplicate.py
@@ -0,0 +1,14 @@
+# Given a list of n numbers, determine if it contains any duplicate numbers.
+
+def find_duplicates(arr):
+    ht = {}
+    for item in arr:
+        if item in ht:
+            return item
+        else:
+            ht[item] = 1
+
+
+if __name__ == "__main__":
+    arr = [1,2,3,3,4,5,6]
+    print(find_duplicates(arr))
